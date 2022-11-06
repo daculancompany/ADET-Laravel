cb012c03e0130bcb22d02baee5848f03df252fce
Niel daculan <niel.daculan@gmail.com>
2022-11-07 06:20:35 +0800

    first commit

---
+++ database/migrations/2022_11_05_061532_create_cutomers_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('user_id')->unsigned(); 
+            $table->string('fname',100);
+            $table->string('lname',100);
+            $table->string('mname',100)->nullable();
+            $table->string('ph',100)->nullable();
+            $table->foreign('user_id')->references('id')->on('users')->unsigned()->onDelete('cascade'); 
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customers');
+    }
+};
